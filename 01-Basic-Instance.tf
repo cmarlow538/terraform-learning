f93cc56098c539a48b96f649e15ece31670ac44a
Caitlin Marlow <cmarlow538@gmail.com>
2025-05-08 11:17:14 +0100

    Update 01-Basic-Instance.tf

--- 01-Basic-Instance.tf
+++ 01-Basic-Instance.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-reosurce "aws_instance" "my_test_instance"{
-  ami = "ami-0dfe0f1abee59c78d
+resource "aws_instance" "my_test_instance"{
+  ami = "ami-0dfe0f1abee59c78d"
   instance_type = "t2.micro"
 }
