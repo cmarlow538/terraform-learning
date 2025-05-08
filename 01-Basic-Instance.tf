e7e2ea6bedb91732c17d456b4d791ff2ee898f52
Caitlin Marlow <cmarlow538@gmail.com>
2025-05-08 10:52:34 +0100

    Create 01-Basic-Instance.tf

---
+++ 01-Basic-Instance.tf
@@ -0,0 +1,10 @@
+### Task to create a basic AWS EC2 instance
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+reosurce "aws_instance" "my_test_instance"{
+  ami = "ami-0dfe0f1abee59c78d
+  instance_type = "t2.micro"
+}
